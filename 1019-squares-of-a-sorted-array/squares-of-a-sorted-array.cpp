0c61117ebdda7b2b20b2a28dcb8aa1120c648529
Rahul Anand <62982824+zeul22@users.noreply.github.com>
2025-04-26 11:07:50 +0530

    Time: 0 ms (100.00%) | Memory: 31.3 MB (5.57%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -1,12 +1,42 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
+        // O(n2)
+        // vector<int>res(nums.size(),0);
+        // for(int i=0;i<nums.size();i++){
+        //     res[i]=nums[i]*nums[i];
+        // }
+        // sort(res.begin(),res.end());
+        // return res;
 
-        vector<int>res(nums.size(),0);
-        for(int i=0;i<nums.size();i++){
-            res[i]=nums[i]*nums[i];
+        // O(n) : Two Pointers
+
+        int start=0;
+        int end=nums.size() -1;
+        vector<int>res;
+        while(start<=end){
+            if(abs(nums[start])>abs(nums[end])){
+                res.push_back(abs(nums[start]));
+                start++;
+            }
+            else{
+                res.push_back(abs(nums[end]));
+                end--;
+            }
         }
-        sort(res.begin(),res.end());
+        for(int i=0;i<nums.size();i++){
+            // cout << res[i] << " ";
+            // cout << endl;
+            int val=res[i]*res[i];
+            res[i]=val; 
+            // cout << res[i] << " ";
+        }
+        reverse(res.begin(),res.end());
+        cout << endl; 
         return res;
     }
 };
+
+
+// -4,-1,0,3,10
+// 10,4,3,1,0
