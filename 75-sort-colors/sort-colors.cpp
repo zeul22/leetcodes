01f57fd090bc8185b7348eacc898c83eb2267e71
Rahul Anand <62982824+zeul22@users.noreply.github.com>
2025-04-27 17:27:37 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (78.89%) - LeetSync

---
+++ 75-sort-colors/sort-colors.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void mergeSort(vector<int>& nums){
+
+
+
+    }
+    void sortColors(vector<int>& nums) {
+        // merge sort: One Pass Sorting Algorithm, Learn all the sorting algos
+
+        mergeSort(nums);
+
+
+        sort(nums.begin(),nums.end());
+
+
+
+
+
+
+        
+    }
+};
