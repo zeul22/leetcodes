f6a052d1eeca728a3d28c051b84404b56df1faf4
Rahul Anand <62982824+zeul22@users.noreply.github.com>
2025-04-29 12:08:35 +0530

    Time: 78 ms (15.73%) | Memory: 20.2 MB (10.86%) - LeetSync

--- 843-binary-trees-with-factors/binary-trees-with-factors.cpp
+++ 843-binary-trees-with-factors/binary-trees-with-factors.cpp
@@ -11,7 +11,7 @@ public:
 
         int n=arr.size();
         int count=0;
-        for(int i=0;i<n;i++){
+        for(int i=1;i<n;i++){
             for(int j=0;j<i;j++){
                 if(arr[i]%arr[j]==0){
                     int val1=mpp[arr[j]]%MOD;
