462ec8bfd5e63419e3da7525a3d76a4780944f94
Rahul Anand <62982824+zeul22@users.noreply.github.com>
2025-04-29 08:55:43 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (80.87%) - LeetSync

--- 1635-number-of-good-pairs/number-of-good-pairs.cpp
+++ 1635-number-of-good-pairs/number-of-good-pairs.cpp
@@ -1,34 +1,37 @@
 class Solution {
 public:
+    int MOD=1e9+7;
     int factorial(int n) {
     if(n > 1) 
-        return n * factorial(n - 1);
+       return (int)((1LL * (n % MOD) * (factorial(n - 1) % MOD)) % MOD);
     else 
         return 1;
 }
     int numIdenticalPairs(vector<int>& nums) {
         vector<int>res(101,0);
         int count=0;
-        // for(auto it:nums){
-        //     res[it]++;
-        // }
+        for(auto it:nums){
+            res[it]++;
+        }
 
-        // for(int i=0;i<101;i++){
-        //     if(res[i]==nums.size()){
-        //         count+=(int)factorial(res[i])/(2*2);
-        //         break;
-        //     }
-        //     if(res[i]>1){
-        //         count+=(int)factorial(res[i])/(2);
-        //     }
-        // }
-        for(int i=0;i<nums.size();i++){
-            for(int j=0;j<nums.size();j++){
-                if(nums[i]==nums[j] && i<j){
-                    count++;
-                }
+        for(int i=0;i<101;i++){
+            if(res[i]==nums.size()){
+                // count+=(int)(factorial(res[i])/(factorial(2)*factorial(res[i]-2)));
+                count+=(res[i]*(res[i]-1))/2;
+                break;
+            }
+            if(res[i]>1){
+                count+=(res[i]*(res[i]-1))/2;
+                // count+=(int)(factorial(res[i])/(factorial(2)*factorial(res[i]-2)));
             }
         }
+        // for(int i=0;i<nums.size();i++){
+        //     for(int j=0;j<nums.size();j++){
+        //         if(nums[i]==nums[j] && i<j){
+        //             count++;
+        //         }
+        //     }
+        // }
         return count;
         
     }
